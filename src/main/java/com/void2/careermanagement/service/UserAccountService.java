9ce9d3486524d7ad379ec29619bd5b40c57b96dc
choi-gi-hwan <cgh9694@gmail.com>
2024-12-02 00:44:57 +0900

    삭제 기능 구현. dto 카멜케이스 수정. mybatis-config.xml 추가 UserAccountService 로직 일부 수정.

--- src/main/java/com/void2/careermanagement/service/UserAccountService.java
+++ src/main/java/com/void2/careermanagement/service/UserAccountService.java
@@ -19,47 +19,44 @@ public class UserAccountService {
     /**
      * 사용자 정보 업데이트
      * 세션이 존재하지 않을시 false 반환
+     *
      * @return 업데이트 성공 여부
      */
     public boolean updateUser(UserDto user, HttpSession session) {
-        if (session.getAttribute("user") != null) {
-            UserDto userSession = (UserDto) session.getAttribute("user");
-            String session_id = userSession.getUserId();
-            if (session_id.equals(user.getUserId())) {
-                return userAccountDao.userUpdate(user) != 0;
-            }
+        UserDto userSession = (UserDto) session.getAttribute("user");
+        if (userSession == null) {
+            return false;
         }
-        return false;
+        if (!userSession.getUserId().equals(user.getUserId())) {
+            return false;
+        }
+        return userAccountDao.userUpdate(user) != 0;
     }
 
     /**
      * 사용자 삭제
      * 세션이 존재하지 않을시 false 반환
-     *  @return 업데이트 성공 여부
+     * @return 업데이트 성공 여부
      */
     public boolean deleteUser(String userId, HttpSession session) {
-        if (session.getAttribute("user") != null) {
-            UserDto userSession = (UserDto) session.getAttribute("user");
-            String session_id = userSession.getUserId();
-            if (session_id.equals(userId)) {
-                if(userAccountDao.userDelete(userId) != 0){
-                    session.removeAttribute("user");
-                    return true;
-                }
-            }
+
+        UserDto userSession = (UserDto) session.getAttribute("user");
+        if (userSession == null) {
+            return false;
         }
-//        UserDto userSession = (UserDto) session.getAttribute("user");
-//        if (userSession != null && userSession.getUserId().equals(userId)) {
-//
-//            String session_id = userSession.getUserId();
-//            if (session_id.equals(userId)) {
-//                if(userAccountDao.userDelete(userId) != 0){
-//                    session.removeAttribute("user");
-//                    return true;
-//                }
-//            }
-//        }
+        if (!userSession.getUserId().equals(userId)) {
+            return false;
+        }
+        if (userAccountDao.userDelete(userId) != 0) {
+            session.removeAttribute("user");
+            return true;
+        }
         return false;
+//    if (userAccountDao.userDelete(userId) != 0) {
+//        session.removeAttribute("user");
+//        return true;
+//    }
+//        return false;
     }
 }
 
