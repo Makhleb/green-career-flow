fb5bd4373cf67116d1d812a8fbfd101c748728f2
Choi Yoonseo <chl_2001@naver.com>
2024-12-02 11:53:11 +0900

    구직자 커뮤니티 게시글 조회

---
+++ src/main/java/com/void2/careermanagement/dao/BoardDao.java
@@ -0,0 +1,14 @@
+package com.void2.careermanagement.dao;
+
+import com.void2.careermanagement.dto.BoardDto;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface BoardDao {
+
+    public List<BoardDto> getList();
+    public BoardDto getBoard(int communityNo);
+
+}
---
+++ src/main/java/com/void2/careermanagement/service/BoardService.java
@@ -0,0 +1,25 @@
+package com.void2.careermanagement.service;
+
+import com.void2.careermanagement.dao.BoardDao;
+import com.void2.careermanagement.dto.BoardDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BoardService {
+
+    @Autowired
+    BoardDao boardDao;
+
+    public List<BoardDto> getList() {
+        List<BoardDto> list = boardDao.getList();
+        return list;
+    }
+
+    public BoardDto getBoard(int communityNo) {
+        BoardDto board = boardDao.getBoard(communityNo);
+        return board;
+    }
+}
