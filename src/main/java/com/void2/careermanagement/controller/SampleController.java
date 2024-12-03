d811865bccd6216075cbe2f4bfd7be36b384fefd
cgh <cgh9694@gmail.com>
2024-12-03 15:05:53 +0900

    기업 회원가입 완료

--- src/main/java/com/void2/careermanagement/controller/SampleController.java
+++ src/main/java/com/void2/careermanagement/controller/SampleController.java
@@ -1,8 +1,12 @@
 package com.void2.careermanagement.controller;
 
 
+import com.void2.careermanagement.dto.CompanyDto;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import java.util.Base64;
 
 @Controller
 public class SampleController {
