440505d7c5d6695ef74c5727f400e82730151fe0
Choi Yoonseo <chl_2001@naver.com>
2024-12-04 20:56:47 +0900

    기업 마이페이지 중간저장

--- src/main/java/com/void2/careermanagement/controller/MyPageController.java
+++ src/main/java/com/void2/careermanagement/controller/MyPageController.java
@@ -1,15 +1,21 @@
 package com.void2.careermanagement.controller;
 
+import com.void2.careermanagement.dto.CompanyDto;
 import com.void2.careermanagement.dto.UserDto;
 import com.void2.careermanagement.dto.response.MyPageScrapDto;
 import com.void2.careermanagement.dto.response.ResumeResponseDto;
 import com.void2.careermanagement.service.MyPageService;
+import com.void2.careermanagement.util.SessionUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -22,20 +28,45 @@ public class MyPageController {
     @Autowired
     private MyPageService myPageService;
 
-    @RequestMapping("/profile")
-    public String profile(HttpSession session, Model model) {
-        UserDto user = (UserDto) session.getAttribute("user");
-        String userId = user.getUserId(); // UserDto 객체의 userId 필드 접근
-        List<ResumeResponseDto> resumeList = myPageService.MyPageResumeListById(userId);
-        List<MyPageScrapDto> scrapList = myPageService.MyPageScrapListById(userId);
-//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
-        int resumeSize = resumeList.size();
-        int scrapSize = scrapList.size();
-        model.addAttribute("resumeSize", resumeSize);
-        model.addAttribute("scrapSize", scrapSize);
-        model.addAttribute("resumeList", resumeList);
-        model.addAttribute("scrapList", scrapList);
-        return "/mypage/user-mypage";
+//    @RequestMapping("/profile")
+//    public String profile(HttpSession session, Model model) {
+//        UserDto user = (UserDto) session.getAttribute("user");
+//        String userId = user.getUserId(); // UserDto 객체의 userId 필드 접근
+//        List<ResumeResponseDto> resumeList = myPageService.MyPageResumeListById(userId);
+//        List<MyPageScrapDto> scrapList = myPageService.MyPageScrapListById(userId);
+//        int resumeSize = resumeList.size();
+//        int scrapSize = scrapList.size();
+//        model.addAttribute("resumeSize", resumeSize);
+//        model.addAttribute("scrapSize", scrapSize);
+//        model.addAttribute("resumeList", resumeList);
+//        model.addAttribute("scrapList", scrapList);
+//        return "/mypage/user-mypage";
+//    }
+
+    @GetMapping("/profile")
+    public String profile(HttpSession session, Model model, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        if(SessionUtil.sessionUserCheckRedirectLogin(session, request, response)) return null;
+        String userType = session.getAttribute("userType").toString();
+        String returnUrl = "";
+        Object sessionUser = session.getAttribute("user");
+        if(userType.equals("U")) {
+            UserDto user = (UserDto) sessionUser;
+            String userId = user.getUserId(); // UserDto 객체의 userId 필드 접근
+            List<ResumeResponseDto> resumeList = myPageService.MyPageResumeListById(userId);
+            List<MyPageScrapDto> scrapList = myPageService.MyPageScrapListById(userId);
+            int resumeSize = resumeList.size();
+            int scrapSize = scrapList.size();
+            model.addAttribute("resumeSize", resumeSize);
+            model.addAttribute("scrapSize", scrapSize);
+            model.addAttribute("resumeList", resumeList);
+            model.addAttribute("scrapList", scrapList);
+            returnUrl = "/mypage/user-mypage";
+        } else if (userType.equals("C")) {
+            CompanyDto user = (CompanyDto) sessionUser;
+            String companyId = user.getCompanyId();
+            returnUrl = "/mypage/company-mypage";
+        }
+        return returnUrl;
     }
 
     @RequestMapping("/proposal")
