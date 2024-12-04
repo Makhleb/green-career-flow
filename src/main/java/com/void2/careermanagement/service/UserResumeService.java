1eca47993a96d20408a229a50d8470a42796833d
irem2493 <irem2493@naver.com>
2024-12-04 15:05:48 +0900

    22

--- src/main/java/com/void2/careermanagement/service/UserResumeService.java
+++ src/main/java/com/void2/careermanagement/service/UserResumeService.java
@@ -1,28 +1,15 @@
 package com.void2.careermanagement.service;
 
-import com.void2.careermanagement.dao.UserResumeDao;
 import com.void2.careermanagement.dto.response.UserResponseDto;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 /**
- * Created on 2024-12-03 by 안제연
+ * Created on 2024-12-04 by 안제연
  */
 @Service
 public class UserResumeService {
-
-    private final UserResumeDao userResumeDao;
-    @Autowired
-    public UserResumeService(UserResumeDao userResumeDao) {this.userResumeDao = userResumeDao;}
-
-    public List<UserResponseDto> getUserReumeList(){
-        List<UserResponseDto> list = userResumeDao.getHighLikeUserList();
-        for(UserResponseDto user : list){
-            System.out.println(user + "service");
-        }
-        return userResumeDao.getHighLikeUserList();
+    public List<UserResponseDto> getUserReumeList() {
     }
-
 }
