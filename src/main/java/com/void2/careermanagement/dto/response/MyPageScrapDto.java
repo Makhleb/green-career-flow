af4326be2bcf217e3fa2f03fd0b3289614b272d8
Choi Yoonseo <chl_2001@naver.com>
2024-12-04 17:14:45 +0900

    중간저장 222

--- src/main/java/com/void2/careermanagement/dto/response/MyPageScrapDto.java
+++ src/main/java/com/void2/careermanagement/dto/response/MyPageScrapDto.java
@@ -1,7 +1,25 @@
 package com.void2.careermanagement.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
 /**
  * Created on 2024-12-04 by 최윤서
  */
-public class MyPageScrap {
-}
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MyPageScrapDto {
+        private String userId;         // 사용자 ID
+        private int jobPostNo;         // 공고 번호
+        private String title;          // 공고 제목
+        private LocalDate endDate;     // 공고 마감일
+        private String companyId;         // 회사 ID
+        private String companyName;    // 회사 이름
+
+    }
