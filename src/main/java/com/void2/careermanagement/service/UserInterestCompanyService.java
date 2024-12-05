f40b0f6a3110313d55f9be010abf6c25fbd5e0ea
irem2493 <irem2493@naver.com>
2024-12-05 10:20:03 +0900

    구직자 관심기업 조회(무한 스크롤 적용X) 커밋

---
+++ src/main/java/com/void2/careermanagement/service/UserInterestCompanyService.java
@@ -0,0 +1,23 @@
+package com.void2.careermanagement.service;
+
+import com.void2.careermanagement.dao.UserInterestCompanyDao;
+import com.void2.careermanagement.dto.response.UserInterestCompanyDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created on 2024-12-05 by 안제연
+ */
+@Service
+public class UserInterestCompanyService {
+    private final UserInterestCompanyDao userInterestCompanyDao;
+
+    @Autowired
+    public UserInterestCompanyService(UserInterestCompanyDao userInterestCompanyDao) {
+        this.userInterestCompanyDao = userInterestCompanyDao;
+    }
+    //관심기업 조회
+    public List<UserInterestCompanyDto> getUserInterestCompanyList(String id) {return userInterestCompanyDao.getInterestList(id);}
+}
