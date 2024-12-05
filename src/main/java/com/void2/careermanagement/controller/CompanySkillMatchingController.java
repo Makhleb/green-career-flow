60bccd8272ebc645d0ced75618610399bf0c37dc
irem2493 <irem2493@naver.com>
2024-12-05 19:33:13 +0900

    커밋2

--- src/main/java/com/void2/careermanagement/controller/CompanySkillMatchingController.java
+++ src/main/java/com/void2/careermanagement/controller/CompanySkillMatchingController.java
@@ -1,7 +1,19 @@
 package com.void2.careermanagement.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created on 2024-12-05 by 안제연
  */
+
+@Controller
+@RequestMapping("/skillMatching")
 public class CompanySkillMatchingController {
+    @GetMapping("/company-skill-matching")
+    public String companySkillMatching() {
+        System.out.println("companySkillMatching..");
+        return "/skillMatching/company-skill-matching";
+    }
 }
