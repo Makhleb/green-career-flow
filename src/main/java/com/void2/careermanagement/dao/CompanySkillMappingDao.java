1324f906bb3814cbe95c11e9daab3204016b3f01
irem2493 <irem2493@naver.com>
2024-12-06 12:14:27 +0900

    기업 기술 매칭 리스트 조회 커밋2

---
+++ src/main/java/com/void2/careermanagement/dao/CompanySkillMappingDao.java
@@ -0,0 +1,7 @@
+package com.void2.careermanagement.dao;
+
+/**
+ * Created on 2024-12-06 by 안제연
+ */
+public class CompanySkillMappingDao {
+}
---
+++ src/main/java/com/void2/careermanagement/dto/response/CompanySkillMatcingResponseDto.java
@@ -0,0 +1,7 @@
+package com.void2.careermanagement.dto.response;
+
+/**
+ * Created on 2024-12-06 by 안제연
+ */
+public class CompanySkillMatcingResponseDto {
+}
