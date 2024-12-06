511e704853280e9006499952a063d882dc066c34
irem2493 <irem2493@naver.com>
2024-12-06 09:02:57 +0900

    기업 기술 매칭 리스트 조회 커밋

--- src/main/java/com/void2/careermanagement/controller/CompanySkillMatchingController.java
+++ src/main/java/com/void2/careermanagement/controller/CompanySkillMatchingController.java
@@ -1,8 +1,17 @@
 package com.void2.careermanagement.controller;
 
+import com.void2.careermanagement.service.GubnService;
+import com.void2.careermanagement.type.GroupCode;
+import com.void2.careermanagement.util.SessionUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
 
 /**
  * Created on 2024-12-05 by 안제연
@@ -11,8 +20,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/skillMatching")
 public class CompanySkillMatchingController {
+    private final GubnService gubnService;
+
+    public CompanySkillMatchingController(GubnService gubnService) {this.gubnService = gubnService;}
+
     @GetMapping("/company-skill-matching")
-    public String companySkillMatching() {
+    public String companySkillMatching(Model model, HttpSession session, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        if (SessionUtil.sessionUserCheckRedirectLogin(session, request, response)) return null;
+        model.addAttribute("skillGubnList", gubnService.getGubnList(GroupCode.SKILL.name()));
         System.out.println("companySkillMatching..");
         return "/skillMatching/company-skill-matching";
     }
