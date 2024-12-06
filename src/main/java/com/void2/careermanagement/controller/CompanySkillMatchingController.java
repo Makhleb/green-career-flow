48f0a23a72ced328d2257322f5f4982ac227f14a
irem2493 <irem2493@naver.com>
2024-12-06 10:26:04 +0900

    기업 기술 매칭 리스트 조회 커밋2

--- src/main/java/com/void2/careermanagement/controller/CompanySkillMatchingController.java
+++ src/main/java/com/void2/careermanagement/controller/CompanySkillMatchingController.java
@@ -1,6 +1,8 @@
 package com.void2.careermanagement.controller;
 
+import com.void2.careermanagement.dto.response.UserResponseDto;
 import com.void2.careermanagement.service.GubnService;
+import com.void2.careermanagement.service.UserResumeService;
 import com.void2.careermanagement.type.GroupCode;
 import com.void2.careermanagement.util.SessionUtil;
 import jakarta.servlet.http.HttpServletRequest;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created on 2024-12-05 by 안제연
@@ -21,13 +24,20 @@ import java.io.IOException;
 @RequestMapping("/skillMatching")
 public class CompanySkillMatchingController {
     private final GubnService gubnService;
+    private final UserResumeService userResumeService;
 
-    public CompanySkillMatchingController(GubnService gubnService) {this.gubnService = gubnService;}
+    public CompanySkillMatchingController(GubnService gubnService, UserResumeService userResumeService) {
+        this.gubnService = gubnService;
+        this.userResumeService = userResumeService;
+    }
 
     @GetMapping("/company-skill-matching")
     public String companySkillMatching(Model model, HttpSession session, HttpServletRequest request, HttpServletResponse response) throws IOException {
         if (SessionUtil.sessionUserCheckRedirectLogin(session, request, response)) return null;
+        String companyId = SessionUtil.getSessionUserId(session);
+        List<UserResponseDto> applicantList = userResumeService.getUserResumeListByApplyId(companyId);
         model.addAttribute("skillGubnList", gubnService.getGubnList(GroupCode.SKILL.name()));
+        model.addAttribute("applicantList", applicantList);
         System.out.println("companySkillMatching..");
         return "/skillMatching/company-skill-matching";
     }
